858d998c9faecae3aef0e7b7689d06e7970509d2
Louis <53097306+louisjdcharles@users.noreply.github.com>
2022-09-23 21:00:05 +0100

    vercel fix second attempt

--- src/lib/trpcClient.ts
+++ src/lib/trpcClient.ts
@@ -2,9 +2,13 @@
 import { createTRPCClient } from '@trpc/client';
 import type { AppRouter } from '../pages/api/trpc/[trpc]';
 
+const getUrl = () => {
+    if (typeof window !== "undefined") return ""; // browser should use relative url
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+    return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+}
+
 export const client = createTRPCClient<AppRouter>({
-    url: process.env.VERCEL_URL
-            ? `${process.env.VERCEL_URL}/api/trpc`
-            : `http://localhost:3000/api/trpc`,
+    url: getUrl() + "/api/trpc"
 
 });
