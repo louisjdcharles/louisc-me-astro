8718e204064ce397a1a26fcef6294c61b4f23a54
Louis <53097306+louisjdcharles@users.noreply.github.com>
2022-09-23 20:54:49 +0100

    vercel fix?

--- src/lib/trpcClient.ts
+++ src/lib/trpcClient.ts
@@ -3,7 +3,8 @@ import { createTRPCClient } from '@trpc/client';
 import type { AppRouter } from '../pages/api/trpc/[trpc]';
 
 export const client = createTRPCClient<AppRouter>({
-    url: process.env.NODE_ENV === 'production'
-            ? `${process.env.VERCEL_URL || ""}/api/trpc`
+    url: process.env.VERCEL_URL
+            ? `${process.env.VERCEL_URL}/api/trpc`
             : `http://localhost:3000/api/trpc`,
+
 });
