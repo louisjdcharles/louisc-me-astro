5e778f117b8def0eb2b1b762271d740e1e1e3a2d
Louis <53097306+louisjdcharles@users.noreply.github.com>
2022-09-24 18:29:38 +0100

    blog implementation, all react code removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ config: { applyBaseStyles: false } })],
+  integrations: [react(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx()],
   output: "server",
   adapter: vercel()
 });
